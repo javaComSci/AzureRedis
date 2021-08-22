b59ca4c91952f8d4ab55836f2ae1fe0285e3142a
javaComSci <iramanat@purdue.edu>
2021-08-21 21:11:34 -0400

    add redis connection

---
+++ AzureRedis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace AzureRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "";
+            var redisConnection = ConnectionMultiplexer.Connect(connectionString);
+
+            IDatabase db = redisConnection.GetDatabase();
+
+            bool wasSet = db.StringSet("favorite:color", "orange");
+
+            redisConnection.Dispose();
+            redisConnection = null;
+            Console.WriteLine("Finishing the addition to redis");
+       
+        }
+    }
+}
